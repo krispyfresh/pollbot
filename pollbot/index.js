7cdde16bcd66b159379e8f11041964de7e849940
Chris Parsons <christopherdotparsons@gmail.com>
2019-04-10 21:13:46 -0500

    ugh

--- pollbot/index.js
+++ pollbot/index.js
@@ -1,13 +1,6 @@
 module.exports = function (context, data) {
     context.log(data);
 
-    // Check if we got first/last properties
-    else {
-        context.res = {
-            status: 400,
-            body: { error: 'Please pass first/last properties in the input object'}
-        };
-    }
 
     context.done();
 }
